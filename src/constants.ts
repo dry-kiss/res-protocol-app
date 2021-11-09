80b1ec20f736f5242d7b7ecbe22b93d0a353ed41
Jason Miller <jeyson.miller66@gmail.com>
2021-11-08 18:10:39 -0800

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,23 +1,23 @@
 export const NETWORKS = {
   localhost: {
-    chainId: 31337,
+    chainId: "0x7a69",
     blockExplorer: "https://alfajores-forno.celo-testnet.org",
     rpcUrl: "http://" + window.location.hostname + ":8545",
     currencySymbol: "CELO",
     currencyName: "Celo",
   },
   "celo-alfajores": {
-    chainId: 44787,
+    chainId: "0xaef3",
     rpcUrl: `https://alfajores-forno.celo-testnet.org`,
     blockExplorer: "https://alfajores-blockscout.celo-testnet.org",
     currencySymbol: "A-CELO",
     currencyName: "Celo",
   },
   celo: {
-    chainId: 42220,
+    chainId: "0xa4ec",
     rpcUrl: `https://forno.celo.org`,
     blockExplorer: "https://explorer.celo.org",
     currencySymbol: "CELO",
     currencyName: "Celo",
   },
-}
+};
