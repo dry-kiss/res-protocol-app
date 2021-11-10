95493b0f75312327b4c7da65e66fd74aca71ca4a
Jason Miller <jeyson.miller66@gmail.com>
2021-11-09 22:10:22 -0800

    add mobile

--- src/pages/home/pages/HomePage.tsx
+++ src/pages/home/pages/HomePage.tsx
@@ -1,5 +1,15 @@
 import { VStack, HStack } from "@chakra-ui/layout"
-import { Heading, Image, Center, Button, Container, IconButton } from "@chakra-ui/react"
+import {
+  Heading,
+  Image,
+  Center,
+  Button,
+  Container,
+  IconButton,
+  useBreakpointValue,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react"
 import {
   faDiscord,
   faFacebook,
@@ -12,136 +22,129 @@ import {
 import { faBookOpen } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import homeImage from "../../../assets/home.svg"
-import { CONTRACTS } from "../../../services/web3/constants"
-
-const metaMaskIcon = "https://cdn.iconscout.com/icon/free/png-256/metamask-2728406-2261817.png"
 
 const HomePage = () => {
-  const requestAddToken = async () => {
-    const _window = window as any
-
-    await _window.ethereum.request({
-      method: "wallet_watchAsset",
-
-      params: {
-        type: "ERC20",
-        options: {
-          address: CONTRACTS.ReSourceToken,
-          symbol: "SOURCE",
-          decimals: 18,
-          image: "http://staging.resource.finance/favicon.ico",
-        },
-      },
-    })
-  }
+  const device = useBreakpointValue({ base: "mobile", md: "desktop" })
+  const isMobile = device === "mobile"
 
   return (
     <Center h="100vh">
-      <HStack w="80%" justifyContent="center">
-        <VStack alignItems="flex-start" w="50%" maxW="420px">
-          <Heading size="xl">Success!</Heading>
-          <Heading size="subtitle" color="gray" mb="1em !important" maxW="350px">
-            Congradulations on being a SOURCE holder, come back soon for staking, underwriting, and
-            more!
-          </Heading>
-          <Button
-            size="lg"
-            colorScheme="blue"
-            justifyContent="space-between"
-            onClick={requestAddToken}
-            w="max-content"
-            rightIcon={<Image width="2em" src={metaMaskIcon} />}
+      <VStack>
+        <Image src={homeImage} display={isMobile ? "" : "none"} />
+        <HStack w="100%" justifyContent="center">
+          <VStack
+            alignItems="flex-start"
+            w={isMobile ? "80%" : "100%"}
+            maxW={isMobile ? "650px" : "420px"}
           >
-            Add SOURCE to wallet
-          </Button>
-          <Button
-            as={"a"}
-            href={"https://resource.finance/"}
-            target={"_blank"}
-            size="lg"
-            colorScheme="blue"
-            justifyContent="space-between"
-            rightIcon={<FontAwesomeIcon icon={faBookOpen} />}
-          >
-            Learn More
-          </Button>
-          <HStack w="100%" justifyContent="space-between" mt="1em !important">
-            <IconButton
-              variant="outline"
+            <Heading size="xl">Success!</Heading>
+            <Heading size="subtitle" color="gray" mb="1em !important" maxW="350px">
+              Congradulations on being a SOURCE holder, come back soon for staking, underwriting,
+              and more!
+            </Heading>
+            <Button
               as={"a"}
-              href={"https://t.me/theresourcenetwork"}
+              href={"https://resource.finance/"}
               target={"_blank"}
-              colorScheme="blue"
-              aria-label="Telegram"
               size="lg"
-              icon={<FontAwesomeIcon icon={faTelegram} />}
-            />
-            <IconButton
-              variant="outline"
-              as={"a"}
-              href={"https://discord.com/invite/UuTCRuuZMP"}
-              target={"_blank"}
               colorScheme="blue"
-              aria-label="Discord"
-              size="lg"
-              icon={<FontAwesomeIcon icon={faDiscord} />}
-            />
-            <IconButton
-              variant="outline"
-              as={"a"}
-              href={"https://twitter.com/resourcemutual"}
-              target={"_blank"}
-              colorScheme="blue"
-              aria-label="Twitter"
-              size="lg"
-              icon={<FontAwesomeIcon icon={faTwitter} />}
-            />
-            <IconButton
-              variant="outline"
-              as={"a"}
-              href={"https://www.facebook.com/resourcenetwork1"}
-              target={"_blank"}
-              colorScheme="blue"
-              aria-label="Facebook"
-              size="lg"
-              icon={<FontAwesomeIcon icon={faFacebook} />}
-            />
-            <IconButton
-              variant="outline"
-              as={"a"}
-              href={"https://www.linkedin.com/company/resourcenetwork/"}
-              target={"_blank"}
-              colorScheme="blue"
-              aria-label="Linkedin"
-              size="lg"
-              icon={<FontAwesomeIcon icon={faLinkedin} />}
-            />
-            <IconButton
-              variant="outline"
-              as={"a"}
-              href={"https://github.com/ReSource-Network/"}
-              target={"_blank"}
-              colorScheme="blue"
-              aria-label="Github"
-              size="lg"
-              icon={<FontAwesomeIcon icon={faGithub} />}
-            />
-            <IconButton
-              variant="outline"
-              as={"a"}
-              href={"https://re-source.medium.com/"}
-              target={"_blank"}
-              colorScheme="blue"
-              aria-label="Medium"
-              size="lg"
-              icon={<FontAwesomeIcon icon={faMedium} />}
-            />
-          </HStack>
-        </VStack>
-        <VStack alignItems="flex-start" w="50%">
-          <Image src={homeImage} />
-        </VStack>
-      </HStack>
+              justifyContent="space-between"
+              rightIcon={<FontAwesomeIcon icon={faBookOpen} />}
+            >
+              Learn More
+            </Button>
+            <Wrap w="100%" justifyContent="space-between" mt="1em !important">
+              <WrapItem>
+                <IconButton
+                  variant="outline"
+                  as={"a"}
+                  href={"https://t.me/theresourcenetwork"}
+                  target={"_blank"}
+                  colorScheme="blue"
+                  aria-label="Telegram"
+                  size="lg"
+                  icon={<FontAwesomeIcon icon={faTelegram} />}
+                />
+              </WrapItem>
+              <WrapItem>
+                <IconButton
+                  variant="outline"
+                  as={"a"}
+                  href={"https://discord.com/invite/UuTCRuuZMP"}
+                  target={"_blank"}
+                  colorScheme="blue"
+                  aria-label="Discord"
+                  size="lg"
+                  icon={<FontAwesomeIcon icon={faDiscord} />}
+                />
+              </WrapItem>
+              <WrapItem>
+                <IconButton
+                  variant="outline"
+                  as={"a"}
+                  href={"https://twitter.com/resourcemutual"}
+                  target={"_blank"}
+                  colorScheme="blue"
+                  aria-label="Twitter"
+                  size="lg"
+                  icon={<FontAwesomeIcon icon={faTwitter} />}
+                />
+              </WrapItem>
+              <WrapItem>
+                <IconButton
+                  variant="outline"
+                  as={"a"}
+                  href={"https://www.facebook.com/resourcenetwork1"}
+                  target={"_blank"}
+                  colorScheme="blue"
+                  aria-label="Facebook"
+                  size="lg"
+                  icon={<FontAwesomeIcon icon={faFacebook} />}
+                />
+              </WrapItem>
+              <WrapItem>
+                <IconButton
+                  variant="outline"
+                  as={"a"}
+                  href={"https://www.linkedin.com/company/resourcenetwork/"}
+                  target={"_blank"}
+                  colorScheme="blue"
+                  aria-label="Linkedin"
+                  size="lg"
+                  icon={<FontAwesomeIcon icon={faLinkedin} />}
+                />
+              </WrapItem>
+              <WrapItem>
+                <IconButton
+                  variant="outline"
+                  as={"a"}
+                  href={"https://github.com/ReSource-Network/"}
+                  target={"_blank"}
+                  colorScheme="blue"
+                  aria-label="Github"
+                  size="lg"
+                  icon={<FontAwesomeIcon icon={faGithub} />}
+                />
+              </WrapItem>
+              <WrapItem>
+                <IconButton
+                  variant="outline"
+                  as={"a"}
+                  href={"https://re-source.medium.com/"}
+                  target={"_blank"}
+                  colorScheme="blue"
+                  aria-label="Medium"
+                  size="lg"
+                  icon={<FontAwesomeIcon icon={faMedium} />}
+                />
+              </WrapItem>
+            </Wrap>
+          </VStack>
+          <VStack alignItems="flex-start" w="50%" display={isMobile ? "none" : ""}>
+            <Image src={homeImage} />
+          </VStack>
+        </HStack>
+      </VStack>
     </Center>
   )
 }
