7f6a02306560ff10a4abc062e86c24c88d97d9c3
Jason Miller <jeyson.miller66@gmail.com>
2021-12-10 00:19:33 -0800

    Add start to lock schedule view

--- src/components/wallet/WalletInfo.tsx
+++ src/components/wallet/WalletInfo.tsx
@@ -1,5 +1,18 @@
 import { Box, BoxProps, Text } from "@chakra-ui/layout"
-import { Center, HStack, Tooltip } from "@chakra-ui/react"
+import {
+  Center,
+  HStack,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Tooltip,
+  useBreakpointValue,
+  useDisclosure,
+} from "@chakra-ui/react"
 import { faLock } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { ethers } from "ethers"
@@ -13,6 +26,19 @@ import { formatEther } from "../../services/web3/utils/ethers"
 import colors from "../../theme/foundations/colors"
 import { useManagedCountUp } from "../../utils/useManagedCountUp"
 import { GlyphLabel } from "../glyph/SourceGlyphLabel"
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Legend,
+} from "chart.js"
+import faker from "faker"
+import { Line } from "react-chartjs-2"
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Legend)
 
 const BALANCE_REF = "BALANCE_REF"
 const LOCKED_REF = "LOCKED_REF"
@@ -20,6 +46,10 @@ const LOCKED_REF = "LOCKED_REF"
 const WalletInfo = ({ ...rest }: BoxProps) => {
   const balance = useSourceBalance()
   const locked = useLockedSourceBalance()
+  const { onOpen, onClose, isOpen } = useDisclosure()
+
+  const device = useBreakpointValue({ base: "mobile", md: "desktop" })
+  const isMobile = device === "mobile"
 
   useManagedCountUp({
     ref: BALANCE_REF,
@@ -31,25 +61,84 @@ const WalletInfo = ({ ...rest }: BoxProps) => {
     end: Number(formatEther(locked)),
   })
 
+  const labels = ["January", "February", "March", "April", "May", "June", "July"]
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  }
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  }
+
   const showLock = Number(formatEther(locked)) > 0
 
   return (
     <Box {...rest}>
-      <HStack spacing={-14}>
-        <Tooltip display={showLock ? "" : "none"} label="Locked SOURCE" shouldWrapChildren>
-          <Center
-            display={showLock ? "" : "none"}
-            {...pillContainerStyles}
-            pr="60px"
-            left={0}
-            borderColor={colors.gray[700]}
+      <HStack spacing={-14} whiteSpace="nowrap">
+        <HStack display={showLock ? "" : "none"}>
+          <Popover
+            isOpen={isOpen}
+            onOpen={onOpen}
+            onClose={onClose}
+            // placement="left"
+            closeOnBlur={false}
           >
-            <Box mx={1}>
-              <Text id={LOCKED_REF} variant="number" mx={2} display="inline" />
-              <FontAwesomeIcon icon={faLock} size="sm" color={colors.gray[700]} />
-            </Box>
-          </Center>
-        </Tooltip>
+            <PopoverTrigger>
+              {/* <Tooltip display={showLock ? "" : "none"} label="Locked SOURCE" shouldWrapChildren> */}
+              <Center
+                display={showLock ? "" : "none"}
+                {...pillContainerStyles}
+                pr="60px"
+                left={0}
+                borderColor={colors.gray[700]}
+              >
+                <Box _hover={{ cursor: "pointer" }} mx={1} onClick={onOpen}>
+                  <Text id={LOCKED_REF} variant="number" mx={2} display="inline" />
+                  <FontAwesomeIcon icon={faLock} size="sm" color={colors.gray[700]} />
+                </Box>
+              </Center>
+              {/* </Tooltip> */}
+            </PopoverTrigger>
+            <PopoverContent
+              center
+              w={isMobile ? "90vw" : "50vw"}
+              mr={isMobile ? "0em" : "2em"}
+              ml={isMobile ? "1em" : "2em"}
+              mt="1em"
+            >
+              <PopoverArrow />
+              <PopoverCloseButton />
+              <PopoverHeader>Unlock Schedule</PopoverHeader>
+              <PopoverBody>
+                <Line options={options} data={data} />
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
+        </HStack>
         <Tooltip label="Total SOURCE" shouldWrapChildren>
           <Center {...pillContainerStyles} right={0} borderColor="black">
             <GlyphLabel id={BALANCE_REF} mx={1} />
